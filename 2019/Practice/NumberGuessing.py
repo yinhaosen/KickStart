6e87ced5c9feb0abb06a54cd77346ead4bab10c3
Haosen Yin <yinhaosen@foxmail.com>
2019-03-24 11:23:43 +1100

    finished number guessing

---
+++ 2019/Practice/NumberGuessing.py
@@ -0,0 +1,22 @@
+import sys
+
+def solve(a, b):
+	m = (a + b) // 2
+	print(m)
+
+	sys.stdout.flush()
+	s = input()
+
+	if s == "CORRECT":
+		return
+	elif s == "TOO_SMALL":
+		a = m + 1
+	else:
+		b = m - 1
+	solve(a, b)
+
+T = int(input())                     # Indicate the number of test cases
+for _ in range(T):                   # Throwaway variable similar to the one in haskell
+	a, b = map(int, input().split())
+	_    = int(input())
+	solve(a+1, b)
